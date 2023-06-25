5526c4f27f517b22706ccab8e2baed1c9b063c96
kwizerarichard <127591350+kwizerarichard@users.noreply.github.com>
2023-06-25 12:07:07 +0300

    re- starting TDD cycle 1

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-# Test First TDD Cycle 1
 
-# First test
-
-
-def test():
-    assert multiply(1, 1) == 1
-
-
-# get the test to pass by writing the multiply function.
-
-
-def multiply(a, b):
-    return a * b
