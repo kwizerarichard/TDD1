956996974597bd8e18637ee1841ec30b00a1b03a
kwizerarichard <127591350+kwizerarichard@users.noreply.github.com>
2023-06-26 01:25:47 +0300

    TDD cycle3 test1 failed

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 # Test First TDD cycle 1
 
 
-def test1():
-    assert multiply(1, 1) == 1  # first test for TDD cycle 1 has failed.
+#def test1():
+    #assert multiply(1, 1) == 1  # first test for TDD cycle 1 has failed.
 
 
 # Get the test to pass by writing the multiply function.
@@ -13,10 +13,15 @@ def test1():
 
 # Test first TDD cycle2.
 # Test first TDD cycle2 test1.
-def test2():
-    assert multiply(2, 2) == 4  # first test of cycle 2 failed.
+#def test2():
+    #assert multiply(2, 2) == 4  # first test of cycle 2 failed.
 
 
 # TDD cycle1 and cycle2 should pass the test.
-def multiply(a, b):
-    return a * b
+#def multiply(a, b):
+    #return a * b
+
+#Test first TDD cycle3.
+def test():
+    assert multiply(1,1)==9 # TDD cycle3 test1 failed.
+
