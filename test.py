5481c07844ce264a5da07c36eee0835a325531d1
kwizerarichard <127591350+kwizerarichard@users.noreply.github.com>
2023-06-25 22:06:38 +0300

    test passed by writing the multiply function

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-#Test First TDD cycle 1
-#First  test.
+# Test First TDD cycle 1
+# First  test.
+
 
 def test():
-    assert multiply(1,1) == 1
+    assert multiply(1, 1) == 1  # first test for TDD cycle 1 has failed.
 
+#Get the test to pass by writing the multiply function.
 
-     
+def multiply (a,b):
+    return a * b #After running it, the test passed.
