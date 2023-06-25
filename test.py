f03306e06c29b9375c38f582d0051b0a2a95453c
kwizerarichard <127591350+kwizerarichard@users.noreply.github.com>
2023-06-25 23:41:12 +0300

    first test for TDD cycle 2 passed

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 # Test First TDD cycle 1
-# First  test.
-
 
 def test():
     assert multiply(1, 1) == 1  # first test for TDD cycle 1 has failed.
 
-#Get the test to pass by writing the multiply function.
 
-def multiply (a,b):
-    return a * b #After running it, the test passed.
+# Get the test to pass by writing the multiply function.
+
+def multiply(a, b):
+    return a * b  # After running it, the test passed.
+
+
+#Test first TDD cycle 2.
+def test():
+    assert multiply(2,2)== 4 # first test of TDD of cycle 2 passed.
+
+
+
+
